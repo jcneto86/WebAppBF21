a22f28d0b02ae59631563465fe85009a686c649f
João Carlos Fernandes Neto <wysrar@gmail.com>
2018-10-05 12:12:51 -0400

    v0.2 pages done

--- js/client.js
+++ js/client.js
@@ -1,93 +1,101 @@
-jQuery(function() {
+var baseURL = "http://10.2.10.180:8080";
+var nbRow = 1;
 
-    var Client = {};
-    Client.baseURL = "http://10.2.10.205:8080";
+var serverReturn = {
+    "httpStatus": 3040,
+    "meta": [
+        {
+            "idClient": 1,
+            "name": "Henrique",
+            "age": 25,
+            "gender": "M",
+            "email": "henrique@bf21.com",
+            "phoneNumber": "111111",
+            "height": 170,
+            "weight": 75,
+            "bodyFatPercentage": 45,
+            "bmr": 75,
+            "tdce": 75,
+            "clientGoal": {
+                "idClientGoal": 1,
+                "goal": "Lose weight"
+            },
+            "activityLevel": {
+                "idDailyActivityLevel": 1,
+                "description": "Very Light",
+                "tax": 1.3
+            },
+            "proteinRequirement": {
+                "idProteinRequirement": 1,
+                "description": "Sedentary Adult (RDA)",
+                "tax": 0.4
+            },
+            "creationDate": "02/10/2018 07:48:56"
+        }
+    ],
+    "message": "Client list found successfully."
+}
 
-    Client.clientGetAll = function($callback) {
-        $.ajax({
-            type : "GET",
-            url : this.baseURL + "/client",
-            dataType:'jsonp', //nao é necessário se for no mesmo servidor
-            success : $callback
-        });
-    };
+function getAllClients() {
+    var data = null;
+    var xhr = new XMLHttpRequest();
+    xhr.withCredentials = true;
 
-    Client.clientGetById = function(id, callback) { //op1
-        //Client.clientGetById = function(data, callback) { //op2
-        $.ajax({
-            type : "GET",
-            url : this.baseURL + "/client/" + id,
-
-            //opção 1
-            //https://jsonplaceholder.typicode.com/client/1
-
-            //opção 2
-            //https://jsonplaceholder.typicode.com/client?id=1
-            //url : this.baseURL + "/client/",
-            //data: data,
-
-            dataType:'jsonp', //nao é necessário se for no mesmo servidor
-            success : callback
-        });
-    };
-
-    Client.clientUpdate = function(data, callback) { //op1
-        $.ajax({
-            type : "POST",
-            url : this.baseURL + "/client/",
-            data: data,
-            dataType:'jsonp', //nao é necessário se for no mesmo servidor
-            success : callback
-        });
-    };
-
-
-//chamando a função para carregar os dados na tela
-    function tabelaclient(data) {
-        $.each(data, function (index, value) {
-            var tr = $("<tr />")
-            tr.append('<td>' + value.id + '</td>');
-            tr.append('<td>' + value.title + '</td>');
-            tr.append('<td>' + value.body + '</td>');
-            $("#tabelaclient").append(tr);
-        });
-    }
-
-    function selectclient(data) {
-        $.each(data, function (index, value) {
-            $("#selectclient").append($("<option />").text(value.title));
-        });
-    }
-
-    function carregaPostDetalhes(data){
-        $("#Post").append("<b>" + data.id + "</b>: ");
-        $("#Post").append(data.title);
-    }
-
-    function carregaForm(data){
-        var $form = $("#formPost")
-        $form.find("#id").val(data.id);
-        $form.find("#title").val(data.title);
-    }
-
-    function showMessage(data){
-        $("#msg").show().html("Salvo");
-    }
-
-    //Ações dos botoes
-    $("#btnCarregarTabela").click(function(){
-        Client.clientGetAll(tabelaclient);
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+            data = this.responseText;
+        }
     });
+    xhr.open("GET", baseURL + "/client/list");
+    xhr.setRequestHeader("Cache-Control", "no-cache");
+    xhr.setRequestHeader("Postman-Token", "ead70657-64e4-4842-8bad-8c3681ad9d6e");
 
-    $("#btnCarregarSelect").click(function(){
-        Client.clientGetAll(selectclient);
+    // xhr.send(data);
+    // var array = JSON.parse(xhr.responseText);
+    // data = this.responseText;
+    console.log(this.responseText);
+    // return array;
+}
+
+getAllClients();
+
+function listClient(data) {
+    var arrayClients = JSON.parse(data);
+    arrayClients.meta.forEach(function (element) {
+        if (document.getElementById(nbRow) != null) {
+            var row = $("<div class=\"row space-row-projet\"></div>");
+            row.id = nbRow;
+            var col = $("<div class=\"col\"></div>");
+            var card = $("<div class=\"card-element box-shadow-android\"></div>");
+            if (document.getElementById(nbRow).getElementsByTagName("h6").length < 3) {
+                card.append('<input type="hidden" value="' + element.idClient + ' + ">');
+                card.append('<h6>' + element.name + '</h6>');
+                card.append('<div class="card-element-bottom-bar text-center">' +
+                    '<button class="btn bg-transparent"><i class="fas fa-pen"></i></button> ' +
+                    '<button class="btn bg-transparent"><i class="fas fa-trash-alt"></i></button> ' +
+                    '</div>');
+                col.append(card);
+                row.append(col);
+                $("#listClient").append(row);
+            } else {
+                var row = $("<div class=\"row space-row-projet\"></div>");
+                var col = $("<div class=\"col\"></div>");
+                var card = $("<div class=\"card-element box-shadow-android\"></div>");
+                nbRow++;
+                row.id = nbRow;
+                card.append('<input type="hidden" value="' + element.idClient + ' + ">');
+                card.append('<h6>' + element.name + '</h6>');
+                card.append('<div class="card-element-bottom-bar text-center">' +
+                    '<button class="btn bg-transparent"><i class="fas fa-pen"></i></button> ' +
+                    '<button class="btn bg-transparent"><i class="fas fa-trash-alt"></i></button> ' +
+                    '</div>');
+                col.append(card);
+                row.append(col);
+                $("#listClient").append(row);
+            }
+        }
     });
+}
 
-    $("#btnSalvar").click(function(){
-        var data = $("#formPost").serialize();
-        Client.clientUpdate(data, showMessage)
-    })
 
-    //executa função ao carregar a pagina
-    Client.clientGetById(1, carregaForm); //Client.clientGetById(1, carregaPostDetalhes);
-})();
